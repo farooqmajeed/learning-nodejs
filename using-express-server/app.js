6bdcfcd4c977de11bf952c32f984f859eef4bab2
farooqmajeed <farooq306@hotmail.com>
2017-05-01 20:47:58 +0500

    Using Express server

--- using-express-server/app.js
+++ using-express-server/app.js
@@ -2,9 +2,17 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Learning NodeJs');
+    res.send('GOt a get request for Home');
+});
+app.get('/aboutus', function(req, res){
+    console.log(" Got a get request for About us")
+    res.send('About us');
 });
 
+app.get('/*list', function(req, res){
+    console.log(" Got a get request for List ")
+    res.send('Depart List');
+});
 var server = app.listen(7000, function(){
     var host = server.address().address ;
     var port = server.address().port;
